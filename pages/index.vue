759f7adfe61a84d05aa3f11f4e4c3639f7544c61
Daniel Rangel <danielmmrangel@gmail.com>
2025-06-16 03:05:08 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <nav class="
+      mx-auto flex max-w-7xl
+      items-center justify-between p-6
+      lg:px-8"
+      aria-label="Global"
+    >
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+        </a>
+      </div>
+      <div>Início</div>
+      <div>Trabalhos</div>
+      <div>Contato</div>
+    </nav>
+  </header>
+  <h1>Seja bem-vindo ao meu portifolio!</h1>
+  <h2>Daniel Rangel</h2>
+
+  <UButton icon="i-lucide-rocket" color="secondary" variant="subtle">Trabalhos</UButton>
+</template>
